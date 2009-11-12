387f4d3545643f6b0c69bb2ac6800b2541ef9200
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-12 14:55:55 -0600

    Messed up the route again I think.
    git-svn-id: http://svn.symfony-project.com/plugins/pkBlogPlugin/trunk@23840 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkCalendar/templates/_upcomingEvents.php
+++ modules/pkCalendar/templates/_upcomingEvents.php
@@ -3,7 +3,7 @@
 		<li>
 			<h4><?php echo $pk_blog_event->getTitle() ?></h4>
 			<p><?php echo $pk_blog_event->getExcerpt() ?></p>
-			<p><?php echo link_to('Read More', 'pk_blog_calendar_post', $pk_blog_event, array('class' => 'pk-blog-more')) ?></p>
+			<p><?php echo link_to('Read More', 'pk_calendar_post', $pk_blog_event, array('class' => 'pk-blog-more')) ?></p>
 		</li>
 	<?php endforeach ?>
 </ul>
