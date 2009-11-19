3e872879cb57cd93f94b5c9fc521a8aa9d9deec2
jakehiller <jakehiller@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-19 11:10:06 -0600

    changed basic structure and class names
    git-svn-id: http://svn.symfony-project.com/plugins/pkBlogPlugin/trunk@24174 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkCalendar/templates/_upcomingEvents.php
+++ modules/pkCalendar/templates/_upcomingEvents.php
@@ -1,9 +1,21 @@
-<ul class="pk-blog-recentposts">
+<div class="pk-calendar-upcomingevents">
 	<?php foreach ($pk_blog_events as $pk_blog_event): ?>
-		<li>
-			<h4><?php echo $pk_blog_event->getTitle() ?></h4>
-			<p><?php echo $pk_blog_event->getExcerpt() ?></p>
-			<p><?php echo link_to('Read More', 'pk_calendar_post', $pk_blog_event, array('class' => 'pk-blog-more')) ?></p>
-		</li>
+		
+		<div class="pk-calendar-event">
+			<div class="pk-calendar-meta">
+				<ul>
+					<li class="pk-calendar-date"><?php echo date('l', strtotime($pk_blog_event->getStartDate())) ?></li>
+					<li><?php echo date('F jS Y', strtotime($pk_blog_event->getStartDate())) ?></li>
+					<li><?php echo date('gA', strtotime($pk_blog_event->getStartDate())) ?></li>
+				</ul>
+			</div>
+		
+			<div class="pk-calendar-event-body">
+				<h4><?php echo $pk_blog_event->getTitle() ?></h4>
+				<p><?php echo $pk_blog_event->getExcerpt() ?></p>
+				<p><?php echo link_to('Read More', 'pk_calendar_post', $pk_blog_event, array('class' => 'pk-blog-more')) ?></p>
+			</div>
+		</div>
+		
 	<?php endforeach ?>
-</ul>
+</div>
