bffe1b2510a4765510b52fd96e8b938d2197d4e5
dordille <dordille@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-23 16:15:12 -0600

    Adding tag typeahead support for pkBlogEvent [BugzID: 1770]
    git-svn-id: http://svn.symfony-project.com/plugins/pkBlogPlugin/trunk@24300 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkBlogEventAdmin/templates/_form_header.php
+++ modules/pkBlogEventAdmin/templates/_form_header.php
@@ -1,1 +1,8 @@
 <?php echo link_to('Edit Events', '@pk_blog_event_admin', array('class' => 'pk-btn icon pk-blog', )) ?>
+
+<script src='/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js'></script>
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+    pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
+	});
+</script>
