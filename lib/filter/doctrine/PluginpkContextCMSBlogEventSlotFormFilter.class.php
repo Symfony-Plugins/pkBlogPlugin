d3d1b7c312eba27a0dfc27c71240f389049bcf90
dordille <dordille@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-24 15:39:23 -0600

    "Adding BlogEventSlot, some changes to css need to be made."
    git-svn-id: http://svn.symfony-project.com/plugins/pkBlogPlugin/trunk@24358 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/doctrine/PluginpkContextCMSBlogEventSlotFormFilter.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginpkContextCMSBlogEventSlot form.
+ *
+ * @package    filters
+ * @subpackage pkContextCMSBlogEventSlot *
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginpkContextCMSBlogEventSlotFormFilter extends BasepkContextCMSBlogEventSlotFormFilter
+{
+}
---
+++ lib/form/doctrine/PluginpkContextCMSBlogEventSlotForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginpkContextCMSBlogEventSlot form.
+ *
+ * @package    form
+ * @subpackage pkContextCMSBlogEventSlot
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginpkContextCMSBlogEventSlotForm extends BasepkContextCMSBlogEventSlotForm
+{
+}
