11daf26c3d1349596026df299a271f02d7cdd303
jakehiller <jakehiller@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-25 10:44:20 -0600

    another small edit to upcoming events, title moved above date in the template
    git-svn-id: http://svn.symfony-project.com/plugins/pkBlogPlugin/trunk@27157 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkCalendar/templates/_upcomingEvents.php
+++ modules/pkCalendar/templates/_upcomingEvents.php
@@ -2,6 +2,7 @@
   <?php foreach ($pk_blog_events as $pk_blog_event): ?>
     
     <div class="pk-calendar-event">
+	    <h3 class="pk-blog-post-title"><?php echo link_to($pk_blog_event->getTitle(), 'pk_calendar_post', $pk_blog_event) ?></h3>
       <div class="pk-calendar-meta">
         <ul>
 	        <li class="pk-calendar-date"><?php echo date('l, F jS Y', strtotime($pk_blog_event->getStartDate())) ?></li>
@@ -14,7 +15,6 @@
       </div>
     
       <div class="pk-calendar-event-body">
-        <h3 class="pk-blog-post-title"><?php echo link_to($pk_blog_event->getTitle(), 'pk_calendar_post', $pk_blog_event) ?></h3>
         <?php if (str_word_count(strip_tags($pk_blog_event->getBody())) > 30): ?>
           <?php echo ($pk_blog_event->getExcerpt()) ? $pk_blog_event->getExcerpt() : $pk_blog_event->getPreview(30) ?>
           <span class='pk-blog-read-more'><?php echo link_to('Read More', 'pk_calendar_post', $pk_blog_event, array('class' => 'pk-blog-more')) ?></span>
